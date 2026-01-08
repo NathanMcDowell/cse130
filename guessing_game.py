3d6112206027153e7a26603169b64aa1a0f0ea59
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2026-01-07 18:49:21 -0700

    The loop runs until the user puts the right number in and has a catch for if non-integers are input

--- guessing_game.py
+++ guessing_game.py
@@ -3,9 +3,25 @@ high_number = 10
 def main():
     target_number = random.choice(range(1, high_number + 1))
     print(target_number)
-    print(f"Pick a number between 1 and {high_number}")
+    print(f"Try to guess a number between 1 and {high_number}")
+    user_number = 0
+    guesses_remaining = 3
+    while user_number != target_number and guesses_remaining > 1:
+        try:
+            user_number = int(input("> "))
+            if user_number == target_number:
+                print("Congratulations! You got the number.")
+            elif user_number > target_number:
+                print("Too high!")
+                
+            elif user_number < target_number:
+                print("Too low!")
+        except ValueError:
+            print("Please give an integer.")
+        
+        
+            
     
-
 
 
 
