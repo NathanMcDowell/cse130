fd00dcd15b9fb2f1034dfe31b9f485ad153a375a
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2026-01-07 18:30:40 -0700

    Randomly chooses a number from a predetermined range

--- guessing_game.py
+++ guessing_game.py
@@ -1,7 +1,10 @@
-
-
+import random
+high_number = 10
 def main():
-    print("Hello World!")
+    target_number = random.choice(range(1, high_number + 1))
+    print(target_number)
+    print(f"Pick a number between 1 and {high_number}")
+    
 
 
 
