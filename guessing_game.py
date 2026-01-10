95276d2147b80206a499aa70020a5cfb4bac6d1a
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2026-01-10 16:22:22 -0700

    Guessing game complete

--- guessing_game.py
+++ guessing_game.py
@@ -2,15 +2,18 @@ import random
 high_number = 10
 def main():
     target_number = random.choice(range(1, high_number + 1))
-    print(target_number)
-    print(f"Try to guess a number between 1 and {high_number}")
     user_number = 0
-    guesses_remaining = 3
+    starting_guesses = high_number//2
+    guesses_remaining = starting_guesses
+    print(f"Debug Target Number: {target_number}")
+    print(f"Try to guess a number between 1 and {high_number}")
+    print(f"You have {starting_guesses} guesses")
     while user_number != target_number and guesses_remaining > 0:
         try:
             user_number = int(input("> "))
             if user_number == target_number:
                 print("Congratulations! You got the number.")
+                print(f"It took you {starting_guesses-guesses_remaining+1} guesses.")
             elif user_number > target_number:
                 guesses_remaining -= 1
                 print(f"Too high! You have {guesses_remaining} guesses left.")
