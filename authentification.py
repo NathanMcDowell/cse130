3929e318debcfcb7d8eefb0ca654117cfd4eed76
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2026-01-16 16:29:07 -0700

    usernames and passwords are put into a list

--- authentification.py
+++ authentification.py
@@ -8,3 +8,19 @@
 #      -a paragraph or two about how the assignment went for you-
 # 5. How long did it take for you to complete the assignment?
 #      -total time in hours including reading the assignment and submitting the program-
+
+import json
+
+def main():
+    with open("Lab02.json", "rt") as filehandle:
+        data = json.load(filehandle)
+
+    user_input = ""
+    while user_input != "q":
+        username_attempt = input("Username: ")
+        # password_attempt = input("Password: ")
+        for i in data["password"]:
+            print(i)
+
+
+main()
