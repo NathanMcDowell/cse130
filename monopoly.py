dc798b278f3444af65a4c7d6f21c1248b347dedb
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2026-01-26 11:40:59 -0700

    asks if they own all the greens

--- monopoly.py
+++ monopoly.py
@@ -13,6 +13,10 @@
 #      -total time in hours including reading the assignment and submitting the program-
 
 def main():
-    print("Hello, World!")
+    owns_all_green = input("Do you own all of the green properties? (y/n) ")
+    if owns_all_green == "y":
+        print("Check individual property information function")
+    else:
+        print("Ya don't own the greens!")
 
 main()
