94df9ea206b4a612f5925d0c75faa3a57f41c9c5
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2026-03-13 15:18:17 -0600

    slight modification to is_leap_year

--- number_of_days.py
+++ number_of_days.py
@@ -1,6 +1,6 @@
 
 def is_leap_year(year):
-    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0) and year > 1752
 
 def complete_month(day_count, day, month, year):
     if month in (1, 3, 5, 7, 8, 10, 12):
