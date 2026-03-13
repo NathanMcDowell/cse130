3059fb747d046ce3449de7393185feca2cab6070
NathanMcDowell <nathanael.a.mcdowell@gmail.com>
2026-03-13 15:03:38 -0600

    number of days complete

--- number_of_days.py
+++ number_of_days.py
@@ -2,6 +2,23 @@
 def is_leap_year(year):
     return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
 
+def complete_month(day_count, day, month, year):
+    if month in (1, 3, 5, 7, 8, 10, 12):
+        days = 31
+    elif month in (4, 6, 9, 11):
+        days = 30
+    else: # February
+        if not is_leap_year(year):
+            days = 28
+        else:
+            days = 29
+    while day <= days:
+        day_count += 1
+        day += 1
+    day = 1
+    month += 1
+    return day_count, day, month, year
+
 def calculate_number_of_days_preset(s_day, s_month, s_year, e_day, e_month, e_year):
     print(f"Start Day: {s_day}.{s_month}.{s_year} - End Day: {e_day}.{e_month}.{e_year}")
 
@@ -11,6 +28,7 @@ def calculate_number_of_days_preset(s_day, s_month, s_year, e_day, e_month, e_ye
     assert s_day > 0, "Day cannot be 0"
     assert s_month > 0, "Month cannot be less than 1"
     assert s_month < 13, "Month cannot be greater than 12"
+    
     if s_year > e_year:
         assert False,"Start day cannot be after end day"
     elif s_year == e_year:
@@ -26,7 +44,7 @@ def calculate_number_of_days_preset(s_day, s_month, s_year, e_day, e_month, e_ye
         if s_day > 30:
             assert False, "Too many days for that month"
     else:
-        if is_leap_year(s_year) == False:
+        if not is_leap_year(s_year):
             if s_day > 28:
                 assert False, "Too many days for that month"
         else:
@@ -39,7 +57,7 @@ def calculate_number_of_days_preset(s_day, s_month, s_year, e_day, e_month, e_ye
         if e_day > 30:
             assert False, "Too many days for that month"
     else:
-        if is_leap_year(e_year) == False:
+        if not is_leap_year(e_year):
             if e_day > 28:
                 assert False, "Too many days for that month"
         else:
@@ -56,63 +74,14 @@ def calculate_number_of_days_preset(s_day, s_month, s_year, e_day, e_month, e_ye
                     day_count += 1
                     s_day += 1
                 # B
-            elif s_month in (1, 3, 5, 7, 8, 10, 12):
-                while s_day <= 31:
-                    day_count += 1
-                    s_day += 1
-                s_day = 1
-                s_month += 1 # C
-
-            elif s_month in (4, 6, 9, 11):
-                while s_day <= 30:
-                    day_count += 1
-                    s_day += 1
-                s_day = 1
-                s_month += 1 # D
-
-            elif s_month == 2:
-                if is_leap_year(s_year):
-                    while s_day <= 29:
-                        day_count += 1
-                        s_day += 1
-                    s_day = 1
-                    s_month += 1 # E
-                else:
-                    while s_day <= 28:
-                        day_count += 1
-                        s_day += 1
-                    s_day = 1
-                    s_month += 1 # F
+            else:
+                day_count, s_day, s_month, s_year = \
+                complete_month(day_count, s_day, s_month, s_year)
         else:
             while s_year < e_year:
                 while s_month <= 12:
-                    if s_month in (1, 3, 5, 7, 8, 10, 12):
-                        while s_day <= 31:
-                            day_count += 1
-                            s_day += 1
-                        s_day = 1
-                        s_month += 1 # G
-                        
-                    elif s_month in (4, 6, 9, 11):
-                        while s_day <= 30:
-                            day_count += 1
-                            s_day += 1
-                        s_day = 1
-                        s_month += 1 # H
-                        
-                    elif s_month == 2:
-                        if is_leap_year(s_year):
-                            while s_day <= 29:
-                                day_count += 1
-                                s_day += 1
-                            s_day = 1
-                            s_month += 1 # I
-                        else:
-                            while s_day <= 28:
-                                day_count += 1
-                                s_day += 1
-                            s_day = 1
-                            s_month += 1 # J
+                    day_count, s_day, s_month, s_year = \
+                    complete_month(day_count, s_day, s_month, s_year)
                 s_month = 1
                 s_year += 1 # K
     print(f"Day Count: {day_count}")
@@ -123,9 +92,8 @@ def calculate_number_of_days_preset(s_day, s_month, s_year, e_day, e_month, e_ye
 # calculate_number_of_days_preset(9, 0, 2001, 27, 5, 2028)
 # calculate_number_of_days_preset(9, 13, 2001, 27, 5, 2028)
 # calculate_number_of_days_preset(0, 12, 2001, 27, 5, 2028)
-calculate_number_of_days_preset(29, 2, 2003, 27, 5, 2028)
-calculate_number_of_days_preset(9, 1, 2001, 8, 1, 2001) #January 9, 2001, January 8, 2001
-calculate_number_of_days_preset(17, 11, 2002, 6, 4, 2004) # Previous week's test
+# calculate_number_of_days_preset(29, 2, 2003, 27, 5, 2028)
+# calculate_number_of_days_preset(9, 1, 2001, 8, 1, 2001) # January 9, 2001, January 8, 2001
 calculate_number_of_days_preset(9, 1, 2001, 9, 1, 2001) 
 calculate_number_of_days_preset(9, 1, 2001, 19, 1, 2001) 
 calculate_number_of_days_preset(9, 1, 2001, 19, 4, 2001)
--- test_stuff.py
+++ test_stuff.py
@@ -1,1 +1,12 @@
+for i in range(10):
+    for i in range(i):
+        print("*", end="")
+    print()
 
+for i in range(10):
+    for j in range(10 - i):
+        print(" ", end="")
+    for j in range(0, i+1):
+        print("*", end="")
+        print("*", end="")
+    print()
